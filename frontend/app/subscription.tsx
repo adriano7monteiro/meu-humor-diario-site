55dbf452435e160dd0252450bd745c6846717559
adriano7monteiro <adriano7monteiro@gmail.com>
2025-10-26 19:33:46 +0000

    auto-commit for eb5ba951-42be-495e-97e4-f343185d3006

--- frontend/app/subscription.tsx
+++ frontend/app/subscription.tsx
@@ -8,6 +8,7 @@ import {
   Alert,
   ActivityIndicator,
   Platform,
+  Linking,
 } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { useRouter } from 'expo-router';
